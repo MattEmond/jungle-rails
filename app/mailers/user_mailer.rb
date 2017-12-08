f56fa6e4b84c1561311cad34e4479f346585f91a
Matthew Emond <matthewemond@Matthews-MacBook-Pro.local>
2017-12-08 12:41:05 -0500

    Set up user_mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,10 @@
 class UserMailer < ApplicationMailer
+
+  default from: "no-reply@jungle.com"
+
+  def order_email(user)
+    @user = user
+    mail(to: @user.email, subject: "Order ID:#{@order.id}")
+ end
+
 end
